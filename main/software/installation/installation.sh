3d0269583c943737c90cb1011d73d7e3601e468c
Westspray12 <francoisgas0909@gmail.com>
2026-05-02 15:15:50 -0400

    Update and rename d to installation.sh

---
+++ main/software/installation/installation.sh
@@ -0,0 +1,5 @@
+echo "Installing file.."
+wget "https://download1585.mediafire.com/h8k4pufvn4mgCVk36Zoq6uv-X9vs0FCdrZgEIq2aTgKT5sw7jPH4P5FXjY7W36MYcJzQnrQTJ-6Eb_nPP6DLPKRuCm0j7Ctzg_5OPLYQdsFv5KFZApusZKb8WzIOaOW7v-iBSauqDHouftQVwIAGGLS-FV04NJhX83ppnuoCII6_HA/oghbtfjn7nxtp6e/density"
+echo "Moving file.."
+mv density /usr/bin
+echo "Finished installation."
